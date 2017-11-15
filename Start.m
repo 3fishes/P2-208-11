2ce0fed8ce448b96ff3d5a105ece764ac2f57f20
Marcus_U <marcusuhthoff@gmail.com>
2017-11-15 16:57:25 -0500

    Added 'Start.m' file

---
+++ Start.m
@@ -0,0 +1,36 @@
+clear all
+close all
+clc
+tic;
+%% Variale initializing
+global widthy 
+global lengthy
+widthy = 5;
+lengthy = 5;
+
+mainCell = cell(widthy,lengthy); %Preallocate for speed
+z = zeros(widthy,lengthy);
+%% filling cells
+for i=1:widthy
+    for j = 1:lengthy
+    struct.convinced = rand*100;
+    struct.A = rand;
+    struct.B = rand;
+    struct.C = rand;
+    mainCell{i,j} = struct;
+    clear struct;
+    end
+end
+summed = 0;
+%% plotting
+surf(z);
+avg = summed /(lengthy*widthy);
+[summed, avg] = plotter(mainCell);
+
+%% ALGORITHMS 
+% - FINDING GROUP LOW
+radius_influence = 1; %
+
+
+
+toc;
