de4a64898711f9f1ae8e04af3785d8810fc5e019
troygiorshev <troygiorshev@gmail.com>
2017-11-22 16:57:11 -0500

    Fix

--- myplotter.m
+++ myplotter.m
@@ -10,13 +10,8 @@ end
 figure(1)
 surf(z);
 axis([1 cols 1 rows 0 100]);
-<<<<<<< HEAD
 %colorbar;
 %view(2);
-h= waitforbuttonpress;
-=======
-view(2);
 h = waitforbuttonpress;
->>>>>>> a696645294b25a58c54f3234bdff26af004a8aab
 
 end
