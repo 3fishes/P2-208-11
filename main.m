bd4fc43fc0ff80e249c7f5a87ab1751e0cd81cdf
Marcus_U <marcusuhthoff@gmail.com>
2017-11-24 10:27:06 -0500

    new main

---
+++ main.m
@@ -0,0 +1,15 @@
+global rows
+global cols
+rows = 5;
+cols = 5;
+for i = 1: rows
+    for j = 1 : cols
+        node.stub = 0.3+.7*rand;
+        node.opin = rand*100;
+        node.agents = rand(1,3);
+        maincell(i,j) = node;
+    end
+end
+while (true)
+maincell = my_test(maincell,5);
+end
