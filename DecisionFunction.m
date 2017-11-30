f16cfc8e96bb8c6160b3bbe7679e2a6b37983aff
JakeStubbs4 <15jps1@queensu.ca>
2017-11-30 18:59:26 -0500

    DecisionFunction

---
+++ DecisionFunction.m
@@ -0,0 +1,23 @@
+function [choice, CurrentOpinion] = DecisionFunction (InitialOpinion, OpinionGoal, CurrentOpinion, maxtime, time)
+
+timeleft = maxtime - time;
+
+if(timeleft == maxtime)
+DesiredRate = ((OpinionGoal - InitialOpinion)/timeleft);
+
+elseif(timeleft < maxtime)
+DesiredRate = ((OpinionGoal - CurrentOpinion)/timeleft);
+end %%Ending above if statement
+
+CurrentOpinion = InitialOpinion + DesiredRate*time;
+
+ActualOpinion = AVGofOpinions;
+
+if (ActualOpinion < CurrentOpinion)
+    choice = 1;
+    
+elseif(CurrentOpinion < ActualOpinion)
+    choice = 0;
+end %%Ending above if statement
+
+end %% Ending Overall Function
