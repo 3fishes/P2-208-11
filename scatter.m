69dcd301244a904297e7bb488bdff4328038f240
JakeStubbs4 <15jps1@queensu.ca>
2017-12-01 11:59:41 -0500

    Scatter Goodness

--- scatter.m
+++ scatter.m
@@ -1,21 +1,22 @@
-function [Agents] = scatter (Agents)
-    agentnum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function [Agents] = scatter (A)
+
+global rows
+global cols
+global agentnum
+
+for i = 1:numel(fieldnames(A))
     
-    for i = 1:numel(fieldnames(A))
-        
+    j = 1 + round(rand()*(rows-1));
+    k = 1 + round(rand()*(cols-1));
+    
+    while (A.(agentnum(i)).grid(j,k).discovered == 1)
         j = 1 + round(rand()*(rows-1));
         k = 1 + round(rand()*(cols-1));
-        
-        while (A.(agentnum(i).grid(j,k).discovered == 1)
-        j = 1 + round(rand()*(rows-1));
-        k = 1 + round(rand()*(cols-1));
-        
-            if (A.(agentnum(i).grid(j,k).discovered == 0)
-            A.(agentnum(i)).location_row = j;
-            A.(agentnum(i)).location_col = k;
-            end% End of If statement
-            
-        end% End of while loop
-        
-    end % End of for loop  
+    end% End of While loop
+    A.(agentnum(i)).location_row = j;
+    A.(agentnum(i)).location_col = k;
+    A.(agentnum(i)).grid(j,k).discovered = 1;
+
+end % End of for loop
+Agents = A;
 end% End of Function
