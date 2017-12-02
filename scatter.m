4e041e544f3fe6f7ab1d813b7d1dcfd2a10443f2
JakeStubbs4 <15jps1@queensu.ca>
2017-12-01 23:50:23 -0500

    Final?

--- scatter.m
+++ scatter.m
@@ -1,4 +1,4 @@
-function [Agents] = scatter (A)
+function [Agents] = scatter (A, maingrid)
 
 global rows
 global cols
@@ -13,10 +13,11 @@ for i = 1:numel(fieldnames(A))
         j = 1 + round(rand()*(rows-1));
         k = 1 + round(rand()*(cols-1));
     end% End of While loop
+    
     A.(agentnum(i)).location_row = j;
     A.(agentnum(i)).location_col = k;
     A.(agentnum(i)).grid(j,k).discovered = 1;
-
+    A.(agentnum(i)).grid(j,k).convinced = maingrid(j,k).opin;
 end % End of for loop
 Agents = A;
 end% End of Function
