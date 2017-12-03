0ba224f90dc4d7904e66536f363331ee3a6f5c83
troygiorshev <troygiorshev@gmail.com>
2017-12-03 15:41:54 -0500

    Fix loop

--- scatter.m
+++ scatter.m
@@ -8,10 +8,12 @@ for i = 1:numel(fieldnames(A))
     
     j = 1 + round(rand()*(rows-1));
     k = 1 + round(rand()*(cols-1));
+    count = 0;
     
-    while (A.(agentnum(i)).grid(j,k).discovered == 1)
+    while (A.(agentnum(i)).grid(j,k).discovered == 1 && count < 5000)
         j = 1 + round(rand()*(rows-1));
         k = 1 + round(rand()*(cols-1));
+        count = count + 1;
     end% End of While loop
     
     A.(agentnum(i)).location_row = j;
